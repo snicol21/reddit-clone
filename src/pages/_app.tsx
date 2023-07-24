d35e1f004629a026935b9399f9ae5dedab822463
snicol21 <spencer.nicol@gmail.com>
2023-07-24 14:18:42 -0600

    implement recoil global state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import { theme } from '~/chakra/theme';
 import { ChakraProvider } from '@chakra-ui/react';
-import Layout from '../components/Layout';
+import { RecoilRoot } from 'recoil';
+import { theme } from '~/chakra/theme';
+import Layout from '~/components/Layout';
 
 import type { AppProps } from 'next/app';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
